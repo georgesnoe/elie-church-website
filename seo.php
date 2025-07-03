f14e3f09b6511b8e19d492ccee0f5c1c26e074d4
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-03 18:46:14 +0000

    Add seo metadata file to be included in views

---
+++ seo.php
@@ -0,0 +1,52 @@
+<!-- SEO Metadata -->
+<meta name="description" content="Site web de l'église méthodiste du Togo, sis à Tokoin Wuiti">
+<meta name="keywords" content="église, méthodiste, togo, tokoin" />
+<meta name="author" content="SOSSOU Mawupénukukpoé" />
+<meta name="robots" content="index, follow" />
+<meta name="googlebot" content="index, follow" />
+<meta name="bingbot" content="index, follow" />
+<link rel="canonical" href="https://exemple.com/" />
+
+<!-- Optimiser pour les réseaux sociaux -->
+<meta property="og:title" content="Église méthodiste du Togo - Tokoin Wuiti" />
+<meta property="og:description" content="Site web de l'église méthodiste du Togo, sis à Tokoin Wuiti" />
+<meta property="og:image" content="https://exemple.com/favicon.ico" />
+<meta property="og:url" content="https://exemple.com/" />
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="Église méthodiste du Togo - Tokoin Wuiti" />
+
+<!-- Optimiser pour X (alias twitter) -->
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="Église méthodiste du Togo - Tokoin Wuiti" />
+<meta name="twitter:description" content="Site web de l'église méthodiste du Togo, sis à Tokoin Wuiti" />
+<meta name="twitter:image" content="https://exemple.com/favicon.ico" />
+
+<!-- Geolocalisation -->
+<meta name="geo.region" content="TG" />
+<meta name="geo.placename" content="Togo" />
+
+<!-- Langue -->
+<link rel="alternate" hreflang="x-default" href="https://exemple.com/favicon.ico" />
+
+<!-- Favicon -->
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="favicon.ico" type="image/x-icon" />
+
+
+<!-- Schema.org -->
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Website",
+        "name": "Église méthodiste du Togo - Tokoin Wuiti",
+        "url": "https://exemple.com/",
+        "image": [
+            "https://exemple.com/favicon.ico",
+        ],
+        "author": {
+            "@type": "Person",
+            "name": "SOSSOU Mawupénukukpoé"
+        }
+    }
+</script>
+<!-- End SEO Metadata -->
