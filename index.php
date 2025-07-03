7981c224c9380e5ef162a5249c9f94a99c48be5d
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-03 18:46:25 +0000

    Setup index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Eglise méthodiste du Togo - Tokoin Wuiti</title>
+    <?php require_once "./seo.php" ?>
+</head>
+
+<body>
+
+</body>
+
+</html>
