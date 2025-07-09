f90f56a79bbdff7f8cf3bec0743926d336aedf60
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-09 21:19:24 +0000

    Update header with a navigable menu

--- parts/user/header.php
+++ parts/user/header.php
@@ -28,6 +28,45 @@
                 </li>
             </ul>
         </nav>
+
+        <!-- Hamburger menu -->
+        <div class="header__hamburger">
+            <svg class="header__menu-open" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 432 384"><path fill="currentColor" d="M0 336v-48h432v48H0zm0-120v-48h432v48H0zM0 48h432v48H0V48z"/></svg>
+        </div>
+
+        <!-- Menu -->
+        <div class="header__menu">
+            <div class="header__menu-control">
+                <svg class="header__menu-close" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 304 384"><path fill="currentColor" d="M299 73L179 192l120 119l-30 30l-120-119L30 341L0 311l119-119L0 73l30-30l119 119L269 43z"/></svg>
+            </div>
+            <nav class="header__menu-nav">
+                <ul class="header__menu-nav-list">
+                    <li class="header__menu-nav-item">
+                        <a href="" class="header__menu-nav-link">Acceuil</a>
+                    </li>
+                    <li class="header__menu-nav-item">
+                        <a href="" class="header__menu-nav-link">Activités</a>
+                    </li>
+                    <li class="header__menu-nav-item">
+                        <a href="" class="header__menu-nav-link">Enseignements</a>
+                    </li>
+                    <li class="header__menu-nav-item">
+                        <a href="" class="header__menu-nav-link">Histoire de l'église</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
 </div>
 <div class="header__backdrop"></div>
+<script>
+    let menuOpen = document.querySelector(".header__menu-open");
+    let menuClose = document.querySelector(".header__menu-close");
+    let menu = document.querySelector(".header__menu");
+    menuOpen.addEventListener("click", () => {
+        menu.style.left = "0";
+    });
+    menuClose.addEventListener("click", () => {
+        menu.style.left = "-100%";
+    });
+</script>
