42b0cf5028edb3da875b1f51a882413b1ec82171
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-10 14:21:52 +0000

    Remove repetitive patterns from structure

--- parts/admin/head.php
+++ parts/admin/head.php
@@ -1,7 +1,7 @@
 <?php include_once "seo.php" ?>
 <!-- Paths here are relative to the database directory -->
-<link rel="stylesheet" href="../parts/static/css/admin/fonts.css" />
-<link rel="stylesheet" href="../parts/static/css/admin/globals.css" />
+<link rel="stylesheet" href="../parts/static/css/fonts.css" />
+<link rel="stylesheet" href="../parts/static/css/globals.css" />
 <link rel="stylesheet" href="../parts/static/css/admin/header.css" />
 <link rel="stylesheet" href="../parts/static/css/admin/main.css" />
 <link rel="stylesheet" href="../parts/static/css/admin/footer.css" />
