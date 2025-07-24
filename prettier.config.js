e03723ce060a6612d0eb5029a847c11fb4880bbc
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-24 20:36:29 +0000

    config: add Prettier configuration for code formatting

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  htmlWhitespaceSensitivity: "ignore",
+};
