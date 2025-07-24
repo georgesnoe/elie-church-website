e4820368e6045c6c8109bda0d54a97969519215a
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-24 21:51:59 +0000

    config: replace eslint.config.js with eslint.config.mjs for improved TypeScript support

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    files: ["src/*.ts", "src/**/*.ts"],
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+);
