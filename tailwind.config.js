da71bda89be934357c323670c61d86d7a0f77df6
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-24 00:00:22 +0000

    config: add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{ts,html}",
+        "./src/index.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
