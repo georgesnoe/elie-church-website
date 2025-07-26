81fb121fd93927dd3e638cff3cd23203b3033d8f
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-26 21:43:27 +0000

    new: redesign footer with new code

--- src/app/components/church-footer/church-footer.ts
+++ src/app/components/church-footer/church-footer.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import {
+  Facebook,
+  Twitter,
+  Youtube,
+  Mail,
+  Phone,
+  MapPin,
+} from 'lucide-angular';
+import { LucideAngularModule } from 'lucide-angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'church-footer',
-  imports: [],
+  imports: [LucideAngularModule, CommonModule],
   templateUrl: './church-footer.html',
-  styleUrl: './church-footer.css',
 })
 export class ChurchFooter {
   currentYear: number = new Date().getFullYear();
+  readonly Facebook = Facebook;
+  readonly Twitter = Twitter;
+  readonly Youtube = Youtube;
+  readonly Mail = Mail;
+  readonly Phone = Phone;
+  readonly MapPin = MapPin;
+
+  socials = [
+    {
+      name: 'Facebook',
+      href: '#',
+      icon: Facebook,
+    },
+    {
+      name: 'Twitter',
+      href: '#',
+      icon: Twitter,
+    },
+    {
+      name: 'YouTube',
+      href: '#',
+      icon: Youtube,
+    },
+  ];
 }
