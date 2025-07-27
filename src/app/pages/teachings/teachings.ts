0ebe2e749fd32cfb2ce7c70574c3e685d5b656c6
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-27 17:00:50 +0000

    new: generate component for teachings page

---
+++ src/app/pages/teachings/teachings.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teachings',
+  imports: [],
+  templateUrl: './teachings.html',
+  styleUrl: './teachings.css',
+})
+export class Teachings {}
