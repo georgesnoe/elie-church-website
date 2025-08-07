1a64d551ef64e4efbb6ff0d51f22abd3dfcd6667
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-07 23:00:14 +0000

    new: create a component to handle events showcase

---
+++ src/components/EventCard.tsx
@@ -0,0 +1,26 @@
+import EventProps from "@/lib/EventProps";
+
+export default function EventCard({event}: {event: EventProps}) {
+  const { title, description, day, date, time, isPast } = event;
+
+  return (
+    <div className={`event-card ${isPast ? 'past' : ''}`}>
+      <div className="event-image-placeholder">
+        {isPast && (
+          <div className="past-event-overlay">
+            <span className="check-icon">✓</span>
+          </div>
+        )}
+      </div>
+      <div className="card-content">
+        <h4 className="event-card-title">{title}</h4>
+        <p className="event-card-description">{description}</p>
+        <div className="event-details">
+          <p className="event-card-day">{day}</p>
+          <p className="event-card-date">{date}</p>
+          <p className="event-card-time">{time}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
