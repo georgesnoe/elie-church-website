495b05cc1d6c5f3c3437d2e5e2257bfac4199c82
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-07 22:38:38 +0000

    update: include TodayEvent component into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import DailyVerse from "@/components/DailyVerse";
+import TodayEvent from "@/components/TodayEvent";
 
 export default function Home() {
   return (
     <main>
       <DailyVerse />
+      <TodayEvent />
     </main>
   )
 }
