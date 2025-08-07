fb57a97e2a1136e90c540eeb6abfd189dd7ab860
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-07 23:00:45 +0000

    new: define props to handle everything related to events

---
+++ src/lib/EventProps.ts
@@ -0,0 +1,11 @@
+type EventProps = {
+  id?: number;
+  title: string;
+  description: string;
+  day: string;
+  date: string;
+  time: string;
+  isPast: boolean;
+};
+
+export default EventProps;
