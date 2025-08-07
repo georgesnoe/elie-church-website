aaf838dfedd299d7da18787c80548441fc22f3aa
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-07 23:02:43 +0000

    update: include week schedule inside home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import DailyVerse from "@/components/DailyVerse";
 import TodayEvent from "@/components/TodayEvent";
+import WeekSchedule from "@/components/WeekSchedule";
 
 export default function Home() {
   return (
     <main>
       <DailyVerse />
       <TodayEvent />
+      <WeekSchedule />
     </main>
   )
 }
